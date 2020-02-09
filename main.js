18b9e86d5efa59304cbe20565e46f7d5de631e35
yuriikhomych23 <yuriikhomych94@gmail.com>
2020-02-09 14:40:07 +0000

    Sample code

---
+++ main.js
@@ -0,0 +1,89 @@
+
+// task2
+// let name = prompt('Введіть ваше імя');
+// if(name == 'Імя'){
+//     alert('Введіть ваш пароль')
+// } else if(name == null) {
+//     alert('Вхід скасовано')
+// } else {
+//     alert('Я вас не знаю')
+// }
+
+
+// task2.1
+// let password = prompt('Введіть пароль');
+// if(password == 'LOGOS') {
+//     alert('Ласкаво просимо')
+// } else if(password == null) {
+//     alert('Вхід скасований')
+// } else {
+//     alert('Пароль невірний')
+// }
+
+
+
+// task 3
+// let months = prompt('');
+// if(months >=1 && months <= 2) {
+//     alert('Зима')
+// } else if(months >= 3 && months <= 5) {
+//     alert('Весна')
+// }else if(months >= 6 && months <= 8) {
+//     alert('Літо')
+// }else if(months >= 9 && months <= 11) {
+//     alert('Осінь')
+// } else if (months == 12) {
+//     alert('Зима')
+// } else {
+//     alert('Неможилво')
+// }
+
+
+
+//task1 
+
+let ask1 = prompt('В якому році закінчилась Перша світова війна?');
+let ask2= prompt('В якому році почалась Перша світова ?');
+let ask3 = prompt('В якому році закінчилась Друга війна?');
+let ask4 = prompt('В якому році почалась світова війна?');
+let ask5 = prompt('В якому році Україна стала незлежною?');
+let ask6 = prompt('В якому почалась війна з Росією');
+let ask7 = prompt('В році закінчилась Перша світова війна?');
+let ask8 = prompt('В якому році  Перша світоваріа війна?');
+let ask9= prompt('В якому році івазакінчилась Перша світова війна?');
+let ask10 = prompt('В якому році закінчилась Перша світова війнаfee?');
+let counter = 0;
+let i = ++counter;
+
+if (ask1 == 1914) {
+    alert(++i)
+} else if(ask2 == 1) {
+    alert(++i)
+}else if(ask2 == 2) {
+    alert(++i)
+}else if(ask2 == 3) {
+    alert(++i)
+}else if(ask2 == 4) {
+    alert(++i)
+}else if(ask2 == 5) {
+    alert(++i)
+}else if(ask2 == 6) {
+    alert(++i)
+}else if(ask2 == 7) {
+    alert(++i)
+}else if(ask2 == 8) {
+    alert(++i)
+}else if(ask2 == 9) {
+    alert(++i)
+} else {
+    alert(i)
+}
+
+
+
+
+
+
+
+
+
