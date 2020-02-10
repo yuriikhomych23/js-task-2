a076a551574733a56b8cac7792400dbe3d8b513d
yuriikhomych23 <yuriikhomych94@gmail.com>
2020-02-10 22:40:50 +0000

    Sample code

--- main.js
+++ main.js
@@ -1,3 +1,63 @@
+//task1 
+
+// let counter = 0;
+// let ask1 = prompt('В якому році появився JavaScript?');
+// if (ask1 == 1995) {
+//     ++counter;
+// } 
+
+// let ask2= prompt('В якому році почалась Перша світова ?');
+// if(ask2 == 2) {
+//    ++counter;
+// }
+
+// let ask3 = prompt('В якому році закінчилась Друга війна?');
+// if(ask3 == 2) {
+//    ++counter;
+// }
+
+// let ask4 = prompt('В якому році почалась світова війна?');
+// if(ask4 == 3) {
+//    ++counter;
+// }
+
+// let ask5 = prompt('В якому році Україна стала незлежною?');
+// if(ask5 == 3) {
+//    ++counter;
+// }
+
+// let ask6 = prompt('В якому почалась війна з Росією');
+// if(ask6 == 4) {
+//    ++counter;
+// }
+
+// let ask7 = prompt('В році закінчилась Перша світова війна?');
+
+// if(ask7 == 5) {
+//    ++counter;
+// }
+
+// let ask8 = prompt('В якому році  Перша світоваріа війна?');
+// if(ask8 == 6) {
+//    ++counter;
+// }
+
+// let ask9= prompt('В якому році івазакінчилась Перша світова війна?');
+// if(ask9 == 7) {
+//    ++counter;
+// }
+
+// let ask10 = prompt('В якому році закінчилась Перша світова війнаfee?');
+// if(ask10 == 8) {
+//    ++counter;
+// }
+
+
+// alert('Сума ваших балів складає: ' + counter);
+
+//питання і відповіді самі звичайні головне що працює 
+
+
 
 // task2
 // let name = prompt('Введіть ваше імя');
@@ -40,58 +100,51 @@
 
 
 
+
+
+
+//task CYCLES
+
+
 //task1 
+// let factorial = +prompt('Введіть число');
+// let result = 1;
 
-let counter = 0;
-let ask1 = prompt('В якому році появився JavaScript?');
-if (ask1 == 1995) {
-    ++counter;
-} 
-let ask2= prompt('В якому році почалась Перша світова ?');
-if(ask2 == 1) {
-   ++counter;
-}
-let ask3 = prompt('В якому році закінчилась Друга війна?');
-if(ask3 == 2) {
-   ++counter;
-}
-let ask4 = prompt('В якому році почалась світова війна?');
+// for(let i = 1; i <= factorial; i++) {
+//     result = result * i;
+// }
+// console.log(result);
 
-if(ask4 == 3) {
-   ++counter;
-}
-let ask5 = prompt('В якому році Україна стала незлежною?');
-if(ask4 == 3) {
-   ++counter;
-}
-let ask6 = prompt('В якому почалась війна з Росією');
-if(ask5 == 4) {
-   counter;
-}
-let ask7 = prompt('В році закінчилась Перша світова війна?');
 
-if(ask6 == 5) {
-   ++counter;
-}
-let ask8 = prompt('В якому році  Перша світоваріа війна?');
-if(ask7 == 6) {
-   ++counter;
-}
-let ask9= prompt('В якому році івазакінчилась Перша світова війна?');
-if(ask8 == 7) {
-   ++counter;
-}
-let ask10 = prompt('В якому році закінчилась Перша світова війнаfee?');
-if(ask9 == 8) {
-   ++counter;
-}
-alert('Сума ваших балів складає: ' + counter);
+//task2 
+// for (let i = 1000; i <= 9999; i += 3) { 
+//     console.log(i)
+// }
+
+
+//task3
+// for (let i = 1; i <= 55; i++) { 
+//     if(i % 2) {
+//         console.log(i)
+//     }
+// }
+
+
+// task4
+// for( let i = 90; i >= 0; i-=5) {
+//         console.log(i)
+// }
+
+
+// task5
+// for (let i = 2; i <= 1048576; i*=2 ) {
+//     console.log(i)
+// } доробити
 
 
 
+// task6
 
+// task7 
 
-
-
-
-
+// task8
