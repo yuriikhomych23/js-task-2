72d2c63e2cc2ead490ff8e894777f2bc9cda5d63
yuriikhomych23 <yuriikhomych94@gmail.com>
2020-02-13 19:43:07 +0000

    Sample code

--- main.js
+++ main.js
@@ -158,12 +158,39 @@
 // task while do while
 
 
+//while
 
-let ask = prompt('pass');
 
-if (pass == 1111) {
-    alert('con')
-} else  {
-    alert('ghefosg')
+let ask = prompt('Введіть пароль');
+let pass = 1111;
+let i = 4;
+
+while(i >= 0) {
+    if ( ask != pass) {
+    alert(`Неправильний пароль. Кількість спроб: ${i}`);
+    ask = prompt('Введіть пароль');
+    i--;
+    } else if( ask == pass) {
+        alert('Вітаємо на сайті');
+        break;
+    } 
 }
 
+
+//do while
+
+// let ask = prompt('Введіть пароль');
+// let pass = 1111;
+// let i = 4;
+
+// do {
+//     if ( ask != pass) {
+//     alert(`Неправильний пароль. Кількість спроб: ${i}`);
+//     ask = prompt('Введіть пароль');
+//     i--;
+//     }else if ( ask == pass) {
+//         alert('Вітаємо на сайті')
+//     }
+// } while( i >= 0);
+
+    
